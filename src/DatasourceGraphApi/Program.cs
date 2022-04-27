9c23108ee5d99e41ca44410d1e46f6a39ff9cd64
Johannes <jnth@universal-robots.com>
2022-04-27 08:24:05 +0200

    add graphql to program

--- src/DatasourceGraphApi/Program.cs
+++ src/DatasourceGraphApi/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+
+builder.Services.AddGraphQLServer();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseRouting()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapGraphQL();
+    });
 
 app.Run();
