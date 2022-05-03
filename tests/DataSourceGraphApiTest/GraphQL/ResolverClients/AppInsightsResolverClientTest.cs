a63445a2f148fad050a60b1922bb07d3def3667e
Johannes <jnth@universal-robots.com>
2022-05-03 10:07:57 +0200

    optimize imports

--- tests/DataSourceGraphApiTest/GraphQL/ResolverClients/AppInsightsResolverClientTest.cs
+++ tests/DataSourceGraphApiTest/GraphQL/ResolverClients/AppInsightsResolverClientTest.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using DatasourceGraphApi;
 using DatasourceGraphApi.GraphQL.ResolverClients;
 using FakeItEasy;
 using FluentAssertions;
