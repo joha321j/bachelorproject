2e25cfd468e6d8dfe168dfedae2cf0463cbcb823
Kasper Malmsiø Hoffmann <43535701+kasperhoffmann94@users.noreply.github.com>
2022-05-30 20:25:35 +0200

    Add resolver for youtube api

---
+++ src/DataSourceGraphApi/GraphQL/ResolverClients/YoutubeResolverClient.cs
@@ -0,0 +1,12 @@
+﻿namespace DataSourceGraphApi.GraphQL.ResolverClients;
+
+public class YoutubeResolverClient
+{
+    private readonly IHttpClientFactory _clientFactory;
+
+    public YoutubeResolverClient(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+}
